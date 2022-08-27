ce1a5350f106659459d1508307ac430603d16cd3
mykim <mykim8710@gmail.com>
2022-08-27 23:08:57 +0900

    [ dev ] ConnectionPool & DataSource - DriverManager -> DataSource를 이용하여 Connecion을 얻음 - 적용 및 TestCode를 통한 검증

---
+++ src/main/java/com/example/jdbc/repository/MemberRepositoryV1.java
@@ -0,0 +1,131 @@
+package com.example.jdbc.repository;
+
+import com.example.jdbc.connection.DBConnectionUtil;
+import com.example.jdbc.domain.Member;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.support.JdbcUtils;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.NoSuchElementException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class MemberRepositoryV1 {
+    private final DataSource dataSource;
+
+    // 회원저장
+    public Member save(Member member) throws SQLException {
+        String sql = "INSERT INTO member(MEMBER_ID, MONEY) VALUES(?, ?)";
+
+        Connection con = null;         // 연결객체
+        PreparedStatement psmt = null; // db에 쿼리를 날리는 객체
+
+        try {
+            con = getConnection();    // DBConnectionUtil 를 통해서 데이터베이스 커넥션을 획득
+            psmt = con.prepareStatement(sql); // 데이터베이스에 전달할 SQL과 파라미터로 전달할 데이터들을 준비
+            psmt.setString(1, member.getMemberId());   // SQL의 첫번째 ? 에 값을 지정, String
+            psmt.setInt(2, member.getMoney());  // SQL의 두번째 ? 에 값을 지정, int
+
+            psmt.executeUpdate();  // Statement 를 통해 준비된 SQL을 커넥션을 통해 실제 데이터베이스에 전달, int를 반환하는데 영향받은 DB row 수를 반환
+
+            return member;
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, psmt, null);
+        }
+    }
+
+    // 회원조회
+    public Member findById(String memberId) throws SQLException {
+        String sql = "SELECT * FROM member WHERE MEMBER_ID = ?";
+
+        Connection con = null;
+        PreparedStatement psmt = null;
+        ResultSet rs = null;
+
+        try{
+            con = getConnection();
+            psmt = con.prepareStatement(sql);
+            psmt.setString(1, memberId);
+
+            rs = psmt.executeQuery();   // select Query, 실행 후 select 쿼리의 결과를 반환
+
+            if(rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("MEMBER_ID"));
+                member.setMoney(rs.getInt("MONEY"));
+
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found, memberId = " +memberId);
+            }
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, psmt, rs);
+        }
+    }
+
+    // 회원수정
+    public void update(String memberId, int money) throws SQLException {
+        String sql = "UPDATE member SET MONEY = ? WHERE MEMBER_ID=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+
+            int resultSize = pstmt.executeUpdate();
+            log.info("resultSize={}", resultSize);
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    // 회원삭제
+    public void delete(String memberId) throws SQLException {
+        String sql = "DELETE FROM member WHERE MEMBER_ID = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            int resultSize = pstmt.executeUpdate();
+            log.info("resultSize={}", resultSize);
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    // Get Connection from DataSource
+    private Connection getConnection() throws SQLException {
+        Connection con = dataSource.getConnection();
+        log.info("get connection = {}, class = {}", con, con.getClass());
+        return con;    // db connection 획득
+    }
+
+    // 리소스 정리 !!
+    private void close(Connection con, Statement stmt, ResultSet rs) {
+        JdbcUtils.closeResultSet(rs);
+        JdbcUtils.closeStatement(stmt);
+        JdbcUtils.closeConnection(con);
+    }
+}
---
+++ src/test/java/com/example/jdbc/repository/MemberRepositoryV1Test.java
@@ -0,0 +1,61 @@
+package com.example.jdbc.repository;
+
+import com.example.jdbc.domain.Member;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static com.example.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+class MemberRepositoryV1Test {
+    MemberRepositoryV1 memberRepositoryV1;
+
+    @BeforeEach // 각 테스트가 실헹전에 호출
+    void beforeEach() {
+        // DriverManagerDataSource - 항상 새로운 커넥션을 획득
+        // DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+
+
+        // 커넥션 풀링: HikariProxyConnection -> JdbcConnection
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        memberRepositoryV1 = new MemberRepositoryV1(dataSource);    // MemberRepositoryV1은 DataSource 의존관계 주입이 필요하다.
+    }
+
+    @Test
+    void crudTest() throws SQLException, InterruptedException {
+        // save, c
+        Member member = new Member("memberV1", 1000);
+        memberRepositoryV1.save(member);
+
+        // findById, r
+        Member findMember = memberRepositoryV1.findById(member.getMemberId());
+        log.info("findMember = {}", findMember);
+        assertThat(findMember).isEqualTo(member);
+
+        // update, u
+        memberRepositoryV1.update(member.getMemberId(), 2000);
+        Member updateMember = memberRepositoryV1.findById(member.getMemberId());
+        assertThat(updateMember.getMoney()).isEqualTo(2000);
+
+        // delete, d
+        memberRepositoryV1.delete(member.getMemberId());
+        assertThatThrownBy(() -> memberRepositoryV1.findById(member.getMemberId())).isInstanceOf(NoSuchElementException.class);
+        //Assertions.assertThrows(NoSuchElementException.class, () -> memberRepositoryV0.findById(member.getMemberId()));
+
+        Thread.sleep(1000);
+    }
+
+}
